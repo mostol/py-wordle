6b704e124e71208e7bc2f3c7b69e27e6f82e3165
Jackson Mostoller <jax.mostoller@gmail.com>
2022-07-30 17:59:16 -0700

    Revert to non-local config settings.

--- db/mysql_repository.py
+++ db/mysql_repository.py
@@ -8,8 +8,8 @@ class MysqlRepository(Repository):
         config = {
             'user': 'root',
             'password': 'root',
-            'host': 'localhost',  # default: 'db'. Local: 'localhost'.
-            'port': '32000',  # Default: '3306'. Local: '32000'.
+            'host': 'db',  # default: 'db'. Local: 'localhost'.
+            'port': '3306',  # Default: '3306'. Local: '32000'.
             'database': 'wordle'
         }
         self.connection = mysql.connector.connect(**config)
